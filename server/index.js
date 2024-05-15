0e42548adb5d721c8db14a056e5ae8c40e4998d1
RENAUD Brevin <renaudbrevin@gmail.com>
2024-05-15 16:59:00 +0200

    :sparkles: work in progress

--- server/index.js
+++ server/index.js
@@ -22,6 +22,10 @@ let deckP2 = [];
 let deckTable = [];
 let colors = ['red', 'yellow', 'green', 'blue'];
 
+let bankP1 = 500;
+let bankP2 = 500;
+let bankRoll = 0;
+
 colors.forEach(color => {
     for(let i = 0; i < 5; i++){
         deck.push({color, number: i});
@@ -29,15 +33,18 @@ colors.forEach(color => {
 })
 
 
-
-
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.broadcast.emit('user connected');
+    
+    // Envoyer l'ID du socket au client lorsqu'il se connecte
+    socket.emit('socketID', socket.id);
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
         socket.broadcast.emit('user disconnected');
     });
+
     socket.on('message', (room, msg) => {
         console.log('message: ' + msg);
         io.emit('message', {room, msg});
@@ -56,12 +63,16 @@ io.on('connection', (socket) => {
         //start game when 2 players join    
         console.log(io.sockets.adapter.rooms.get(room));
         const roomClients = io.sockets.adapter.rooms.get(room);
+        const clients = Array.from(roomClients);
+        const id1 = clients[0];
+        const id2 = clients[1];
+
         if(roomClients.size === 2){
             io.to(room).emit('roomStatus', 'Start game');
             deckP1 = [];
             deckP2 = [];
             deckTable = [];
-            showFlop(room)
+            playGame(room, id1, id2);
         } else {
             io.to(room).emit('roomStatus', 'waiting for player');
         }
@@ -71,6 +82,37 @@ io.on('connection', (socket) => {
         socket.leave(room);
         io.to(room).emit('leave', room);
     });
+
+    socket.on('fold', (room, id) => {
+        console.log('Player ' + currentPlayer + ' fold');
+    });
+    
+    socket.on('bet', (room, id, bet) => {
+        console.log('Player ' + currentPlayer + ' bet ' + bet);
+    });
+    
+
+
+    let currentPlayer = ''; // 1 pour joueur 1, 2 pour joueur 2
+
+    function playGame(room, id1, id2){
+        // Show flop and user card
+        showFlop(room, id1, id2);
+
+        currentPlayer = id1;
+        console.log(id1, id2, currentPlayer);
+        socket.on('playerAction', (room, id, action, price) => {
+            console.log(id);
+            console.log(currentPlayer);
+            if(id == currentPlayer){
+                console.log('Player ' + currentPlayer + ' ' + action);
+            } else {
+                console.log('Not your turn');
+            }
+        });
+    }
+
+
 })
 
 
@@ -81,6 +123,11 @@ server.listen(PORT, () => {
 
 
 function showFlop(room){
+    const roomClients = io.sockets.adapter.rooms.get(room);
+    const clients = Array.from(roomClients);
+    const id1 = clients[0];
+    const id2 = clients[1];
+
     deck = deck.sort(() => Math.random() - 0.5);
     let card = "";
     
@@ -95,5 +142,6 @@ function showFlop(room){
         deckTable.push(card);
     }
 
-    io.to(room).emit('flop', deckP1, deckP2, deckTable);
+    io.to(id1).emit('flop', deckP1[0], deckTable);
+    io.to(id2).emit('flop', deckP2[0], deckTable);
 };
