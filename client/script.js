98f0aea33c0c5834a228a43e787b99d6ff6e2d87
RENAUD Brevin <renaudbrevin@gmail.com>
2024-05-17 17:16:29 +0200

    :rocket: Deployed on main

---
+++ client/script.js
@@ -0,0 +1,209 @@
+let i = 0;
+let room = '';
+let socketid = '';
+let userId = '';
+const text = document.querySelector('textarea');
+const socket = io('http://localhost:3000');
+
+socket.on('connect', () => {
+    console.log('Connected');
+});
+
+socket.on('socketID', (id) => {
+    console.log('Socket ID:', id);
+    userId = id;
+});
+
+socket.on('message', (data) => {
+    if(data.room == room){
+        console.log(data);
+        document.querySelector('.data').innerText = data.msg;
+    }
+});
+socket.on('disconnect', () => {
+    console.log('Disconnected');
+});
+
+let send = () => {
+    i++;
+    socket.emit('message', room, text.number);
+    socket.emit('room', room, text.number);
+}
+
+let join = (roomNumber) => {
+    socket.emit('leave', room);
+    room = roomNumber;
+    socket.emit('join', room);
+}
+
+socket.on('roomStatus', (data) => {
+    console.log(data);
+});
+
+socket.on('preFlop', (userDeck, table) => {
+    updateInfo(table);
+
+    let playerContainer = document.querySelector('.cardPlayerContainer');
+
+    // console.log(playerContainer);
+
+    let cardIndex = 0;
+    for (let i = 0; i < 2; i++) {
+        let myCard = document.createElement('div');
+        myCard.classList.add('card', 'cardPlayer');
+        
+        playerContainer.appendChild(myCard);
+
+        myCard.innerHTML = userDeck[cardIndex].number;
+        myCard.classList.add(userDeck[cardIndex].color);
+        cardIndex++;
+    }
+})
+
+
+socket.on('flop', (table) => {
+    let flop = table.deck;
+
+    updateInfo(table);
+
+
+    let flopContainer = document.querySelector('.containerCard .flop')
+    for(let i = 0; i < flop.length; i++){
+        let card = document.createElement('div');
+        card.innerHTML = flop[i].number;
+        card.classList.add('card', flop[i].color);
+        flopContainer.appendChild(card);
+    }
+});
+
+socket.on('turn', (table) => {
+    let turn = table.deck[table.deck.length - 1]
+
+    updateInfo(table);
+
+    let turnContainer = document.querySelector('.containerCard .turn')
+    let card = document.createElement('div');
+    card.innerHTML = turn.number;
+    card.classList.add('card', turn.color);
+    turnContainer.appendChild(card);
+});
+
+socket.on('river', (table) => {
+    let river = table.deck[table.deck.length - 1]
+
+    updateInfo(table);
+
+    let riverContainer = document.querySelector('.containerCard .river')
+    let card = document.createElement('div');
+    card.innerHTML = river.number;
+    card.classList.add('card', river.color);
+    riverContainer.appendChild(card);
+});
+
+socket.on('showdown', (winner) => {
+    console.log('Winner:', winner);
+});
+
+
+let fold = () => {
+    socket.emit('playerAction', room, userId, 'fold');
+};
+
+let bet = () => {
+    socket.emit('playerAction', room, userId, 'bet');
+};
+
+let check = () => {
+    socket.emit('playerAction', room, userId, 'check');
+};
+
+let call = () => {
+    socket.emit('playerAction', room, userId, 'call');
+};
+
+socket.on('acceptAction', (userId, action, table) => {
+    // console.log(userId, action, table.bank);
+    updateInfo(table);
+});
+
+socket.on('noCheck', () => {
+    console.log('Opponent has bet, you can\'t check');
+    document.getElementById('checkButton').disabled = true;
+    document.getElementById('callButton').disabled = false;
+});
+
+socket.on('noCall', () => {
+    console.log('Opponent has check, you can\'t call');
+    document.getElementById('callButton').disabled = true;
+});
+
+socket.on('noBet', () => {
+    console.log('You don\'t have enough money to bet');
+    document.getElementById('betButton').disabled = true;
+});
+
+socket.on('allIn', () => {
+    console.log('You are all in');
+    let button = document.getElementById('betButton');
+    button.disabled = true;
+    button.textContent = 'All in';
+});
+
+
+function disabledButtons(){
+    document.getElementById('foldButton').disabled = true;
+    document.getElementById('betButton').disabled = true;
+    document.getElementById('checkButton').disabled = true;
+    document.getElementById('callButton').disabled = true;
+}
+
+function ableButtons(){
+    document.getElementById('foldButton').disabled = false;
+    document.getElementById('betButton').disabled = false;
+    document.getElementById('checkButton').disabled = false;
+    document.getElementById('callButton').disabled = false;
+
+    if(player.bet === -1 || player.bet === 0){
+        document.querySelector('#callButton').disabled = true;
+    }
+}
+
+function updateInfo(table){
+    player = table.players.find(player => player.id === userId);
+    opponent = table.players.find(player => player.id !== userId);
+
+    document.querySelector('.opponent .name').innerText = opponent.name + ' : ';
+    document.querySelector('.opponent .bankRoll').innerText = opponent.bank;
+
+    document.querySelector('.player .name').innerText = player.name + ' (You) : ';
+    document.querySelector('.player .bankRoll').innerText = player.bank;
+
+    document.querySelector('.table .bankRoll').innerText = "Pot : " + table.bank;
+
+    document.querySelector('#betButton').textContent = 'Bet ' + table.nextBet;
+    if(player.bet === -1 || player.bet === 0){
+        document.querySelector('#callButton').textContent = 'Call';
+    } else{
+        document.querySelector('#callButton').textContent = 'Call ' + opponent.bet;
+    }
+
+    if(player.isTurn){
+        ableButtons();
+    }else{
+        disabledButtons();
+    }
+}
+
+function deleteAllCards(){
+    document.querySelector('.cardPlayerContainer').innerHTML = '';
+    document.querySelector('.flop').innerHTML = '';
+    document.querySelector('.turn').innerHTML = '';
+    document.querySelector('.river').innerHTML = '';
+}
+
+
+
+socket.on('newGame', (table) => {
+    deleteAllCards();
+    updateInfo(table);
+});
--- server/index.js
+++ server/index.js
@@ -3,6 +3,11 @@ import http from 'http';
 import ip from 'ip';
 import { Server } from 'socket.io';
 import cors from 'cors';
+// var Hand = require('pokersolver').Hand;
+import  pokerSolver  from 'pokersolver';
+
+// Your code using Hand
+
 const app = express();
 const server = http.createServer(app);
 const PORT = 3000;
@@ -16,19 +21,48 @@ app.get('/', (req, res) => {
     res.json('ip address: http://' + ip.address()+':'+PORT);    
 });
 
-let deck = [];
-let deckP1 = [];
-let deckP2 = [];
-let deckTable = [];
-let colors = ['red', 'yellow', 'green', 'blue'];
 
-let bankP1 = 500;
-let bankP2 = 500;
-let bankRoll = 0;
+let deck = [];
+let colors = ['C', 'D', 'H', 'S'];
+
+let table = {
+    players: [],
+    deck: [],
+    bank: 0,
+    nextBet: 50,
+    actualBet: 0,
+    actualTurn: 'preFlop'
+}
+
+let player1 = {
+    name: 'Player 1',
+    id: '',
+    deck: [],
+    bank: 500,
+    bet: 0,
+    isTurn: false
+}
+
+let player2 = {
+    name: 'Player 2',
+    id: '',
+    deck: [],
+    bank: 500,
+    bet: 0,
+    isTurn: false
+}
 
 colors.forEach(color => {
-    for(let i = 0; i < 5; i++){
-        deck.push({color, number: i});
+    for(let i = 1; i < 14; i++){
+        let j = i;
+        let index = 0;
+        if(i > 9){
+            index = i - 10;
+            j = ['T', 'J', 'Q', 'K'][index];
+        } else if(i === 1){
+            j = 'A';
+        }
+        deck.push({color, number: j});
     }
 })
 
@@ -69,10 +103,21 @@ io.on('connection', (socket) => {
 
         if(roomClients.size === 2){
             io.to(room).emit('roomStatus', 'Start game');
-            deckP1 = [];
-            deckP2 = [];
-            deckTable = [];
-            playGame(room, id1, id2);
+            player1.id = id1;
+            player2.id = id2;
+            table.players = [player1, player2];
+
+            // Reset deck, player deck and table deck and bank players
+            table.deck = [];
+            player1.deck = [];
+            player2.deck = [];
+            table.bank = 0;
+            table.nextBet = 50;
+            player1.bank = 500;
+            player2.bank = 500;
+
+
+            showPreFlop(room);
         } else {
             io.to(room).emit('roomStatus', 'waiting for player');
         }
@@ -83,37 +128,83 @@ io.on('connection', (socket) => {
         io.to(room).emit('leave', room);
     });
 
-    socket.on('fold', (room, id) => {
-        console.log('Player ' + currentPlayer + ' fold');
-    });
-    
-    socket.on('bet', (room, id, bet) => {
-        console.log('Player ' + currentPlayer + ' bet ' + bet);
-    });
-    
 
 
-    let currentPlayer = ''; // 1 pour joueur 1, 2 pour joueur 2
 
-    function playGame(room, id1, id2){
-        // Show flop and user card
-        showFlop(room, id1, id2);
+    // Play turn
+    socket.on('playerAction', (room, userId, action) => {
+        // Get user by ID
+        let user = userId === player1.id ? player1 : player2;
+        let otherPlayer = table.players.find(player => player.id !== user.id);
+        let bet = table.nextBet;
+        if(user.isTurn){
+            if(action === 'bet'){
+                if (user.bank <= 0) {
+                    io.to(user.id).emit('noBet');
+                    return;
+                } else if(user.bank < bet){
+                    bet = user.bank;
+                    io.to(user.id).emit('acceptAction', user.id, action, table);
+                } 
 
-        currentPlayer = id1;
-        console.log(id1, id2, currentPlayer);
-        socket.on('playerAction', (room, id, action, price) => {
-            console.log(id);
-            console.log(currentPlayer);
-            if(id == currentPlayer){
-                console.log('Player ' + currentPlayer + ' ' + action);
-            } else {
-                console.log('Not your turn');
+                user.bank -= bet;
+                table.bank += bet;
+
+                user.bet = bet;
+
+                table.nextBet = bet + 50;
+
+                changeturn(user, otherPlayer);
+
+                io.to(room).emit('acceptAction', user.id, action, table);
+                io.to(otherPlayer.id).emit('noCheck');
+
+                console.log('bet')
+            } else if(action === 'fold'){
+                otherPlayer.bank += table.bank;
+                table.bank = 0;
+
+                changeturn(user, otherPlayer);
+
+                io.to(room).emit('acceptAction', user.id, action, table);
+
+                console.log('fold')
+            } else if(action === 'check'){
+                user.bet = -1;
+                
+                changeturn(user, otherPlayer);
+                
+                io.to(room).emit('acceptAction', user.id, action, table);
+                io.to(otherPlayer.id).emit('noCall');
+
+                console.log('check')
+
+                console.log(user.bet + ' ' + otherPlayer.bet)
+                if(user.bet === otherPlayer.bet){
+                    nextCard(room);
+                }
+            } else if(action === 'call'){
+                user.bank -= otherPlayer.bet;
+                table.bank += otherPlayer.bet;
+
+                changeturn(user, otherPlayer);
+
+                io.to(room).emit('acceptAction', user.id, action, table);
+                
+                console.log('call')
+
+                nextCard(room);
             }
-        });
-    }
-
-
+        } else {
+            console.log('Not your turn ' + user.isTurn + otherPlayer.isTurn);
+        }
+    });
 })
+
+function changeturn(player, otherPlayer){
+    player.isTurn = false;
+    otherPlayer.isTurn = true;
+}
 
 
 server.listen(PORT, () => {
@@ -121,27 +212,127 @@ server.listen(PORT, () => {
 })
 
 
-
-function showFlop(room){
+function showPreFlop(room){
     const roomClients = io.sockets.adapter.rooms.get(room);
     const clients = Array.from(roomClients);
     const id1 = clients[0];
     const id2 = clients[1];
 
-    deck = deck.sort(() => Math.random() - 0.5);
+    table.actualTurn = 'PreFlop';
+
+    deck = deck.sort(() => Math.random() - 0.5);    
     let card = "";
-    
-    card = deck.shift();
-    deckP1.push(card);
 
-    card = deck.shift();
-    deckP2.push(card);
-
-    for(let i = 0; i < 3; i++){
+    for(let i = 0; i < 2; i++){
         card = deck.shift();
-        deckTable.push(card);
+        player1.deck.push(card);
     }
 
-    io.to(id1).emit('flop', deckP1[0], deckTable);
-    io.to(id2).emit('flop', deckP2[0], deckTable);
+    for (let i = 0; i < 2; i++){
+        card = deck.shift();
+        player2.deck.push(card);
+    }
+
+    player1.isTurn = true;
+
+    io.to(id1).emit('preFlop', player1.deck, table);
+    io.to(id2).emit('preFlop', player2.deck, table);
 };
+
+
+
+function nextCard(room){
+    //Reset bet
+    table.players.forEach(player => {
+        player.bet = 0;
+    });
+    table.actualBet = 0;
+    table.nextBet = 50;
+    
+    if(table.actualTurn === 'PreFlop'){
+        table.actualTurn = 'Flop';
+        let card = deck.shift();
+        table.deck.push(card);
+        let card2 = deck.shift();
+        table.deck.push(card2);
+        let card3 = deck.shift();
+        table.deck.push(card3);
+        io.to(room).emit('flop', table);
+    }
+    else if(table.actualTurn === 'Flop'){
+        table.actualTurn = 'Turn';
+        let card = deck.shift();
+        table.deck.push(card);
+        io.to(room).emit('turn', table);
+
+    } else if(table.actualTurn === 'Turn'){
+        table.actualTurn = 'River';
+        let card = deck.shift();
+        table.deck.push(card);
+        io.to(room).emit('river', table);
+
+    } else if(table.actualTurn === 'River'){
+        table.actualTurn = 'Showdown';
+        let winner = determineWinner(table.players, table.deck);
+
+        console.log(winner);
+
+        winner.bank += table.bank; // Add the bankroll of the table to the winner's bank
+        io.to(room).emit('showdown', winner);
+
+        setTimeout(() => {
+            // Restore all information for a new game
+            table.players.forEach(player => {
+                player.deck = [];
+                player.bet = 0;
+                player.isTurn = false;
+            });
+
+            table.deck = [];
+            table.bank = 0;
+            table.actualBet = 0;
+            table.nextBet = 50;
+            table.actualTurn = 'preFlop';
+
+            io.to(room).emit('newGame', table);
+            
+            showPreFlop(room);
+        }, 5000)
+    }
+}
+
+
+
+
+
+function determineWinner(players, tableDeck) {
+    let winner = null;
+
+    // Déclaration des tableaux pour les mains de chaque joueur
+    let handP1 = players[0].deck.concat(tableDeck);
+    let handP2 = players[1].deck.concat(tableDeck);
+
+    // Convertir les cartes de chaque main en format 'numéroCouleur'
+    const convertedHands = [handP1, handP2].map(hand => {
+        return hand.map(card => {
+            const number = card.number === 'J' ? 'J' : card.number.toString();
+            return number + card.color;
+        });
+    });
+
+    // Affichage des mains converties dans la console
+    var hand1 = pokerSolver.Hand.solve(convertedHands[0]);
+    var hand2 = pokerSolver.Hand.solve(convertedHands[1]);
+    var winnerHand = pokerSolver.Hand.winners([hand1, hand2])[0];
+
+    if(winnerHand.descr === hand1.descr){
+        winner = players[0];
+    } else if (winnerHand.descr === hand2.descr){
+        winner = players[1];
+    } else {
+        winner = 'Tie';
+    }
+
+    return winner;
+}
+
